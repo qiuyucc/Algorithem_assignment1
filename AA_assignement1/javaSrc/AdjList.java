556b3c341ae64f8c1af033740eeb5d09b739751d
qiuyucc <china.cqy@outlook.com>
2019-04-01 13:15:58 +1100

    uploaded
    Thank you

---
+++ AA_assignement1/javaSrc/AdjList.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Adjacency list implementation for the AssociationGraph interface.
+ *
+ * Your task is to complete the implementation of this class.  You may add methods, but ensure your modified class compiles and runs.
+ *
+ * @author Jeffrey Chan, 2019.
+ */
+public class AdjList extends AbstractAssocGraph
+{
+
+    /**
+	 * Contructs empty graph.
+	 */
+    public AdjList() {
+    	 // Implement me!
+
+    } // end of AdjList()
+
+
+    public void addVertex(String vertLabel) {
+        // Implement me!
+    } // end of addVertex()
+
+
+    public void addEdge(String srcLabel, String tarLabel, int weight) {
+        // Implement me!
+    } // end of addEdge()
+
+
+    public int getEdgeWeight(String srcLabel, String tarLabel) {
+		    // Implement me!
+
+		    // update return value
+		    return EDGE_NOT_EXIST;
+    } // end of existEdge()
+
+
+    public void updateWeightEdge(String srcLabel, String tarLabel, int weight) {
+        // Implement me!
+    } // end of updateWeightEdge()
+
+
+    public void removeVertex(String vertLabel) {
+        // Implement me!
+    } // end of removeVertex()
+
+
+    public List<MyPair> inNearestNeighbours(int k, String vertLabel) {
+        List<MyPair> neighbours = new ArrayList<MyPair>();
+
+        // Implement me!
+
+        return neighbours;
+    } // end of inNearestNeighbours()
+
+
+    public List<MyPair> outNearestNeighbours(int k, String vertLabel) {
+        List<MyPair> neighbours = new ArrayList<MyPair>();
+
+        // Implement me!
+
+        return neighbours;
+    } // end of outNearestNeighbours()
+
+
+    public void printVertices(PrintWriter os) {
+        // Implement me!
+    } // end of printVertices()
+
+
+    public void printEdges(PrintWriter os) {
+        // Implement me!
+    } // end of printEdges()
+
+
+    protected class Node {
+    	public Node() {
+
+    	}
+    }
+} // end of class AdjList
